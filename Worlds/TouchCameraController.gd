79542e6860de3957a2ba04361074b7cbbd0d8e2e
unknown <anthonychi36@gmail.com>
2023-11-05 23:13:39 -0800

    Add mobile control to pan and zoom

---
+++ Worlds/TouchCameraController.gd
@@ -0,0 +1,61 @@
+extends Camera2D
+
+# https://www.youtube.com/watch?v=vnAkooGCLDA
+# all but no rotate feature
+
+var zoomSpeed: float = 0.1
+var panSpeed: float = 1.0
+var rotationSpeed: float = 1.0
+
+var canPan: bool = true
+var canZoom: bool = true
+
+var startZoom: Vector2
+var startDistance: float
+var touchPoints: Dictionary = {}
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		handleTouch(event)
+	elif event is InputEventScreenDrag:
+		handleDrag(event)
+
+func handleTouch(event: InputEventScreenTouch):
+	if event.pressed:
+		touchPoints[event.index] = event.position
+	else:
+		touchPoints.erase(event.index)
+	
+	if touchPoints.size() == 2:
+		var touchPointPositions = touchPoints.values()
+		startDistance = touchPointPositions[0].distance_to(touchPointPositions[1])
+		
+		startZoom = zoom
+	elif touchPoints.size() < 2:
+		startDistance = 0
+
+func handleDrag(event: InputEventScreenDrag):
+	touchPoints[event.index] = event.position
+	
+	if touchPoints.size() == 1:
+		if canPan:
+			offset -= event.relative * panSpeed / zoom.x # normalize depending on zoom
+	
+	elif touchPoints.size() == 2:
+		var touchPointPositions = touchPoints.values()
+		var currentDist = touchPointPositions[0].distance_to(touchPointPositions[1])
+		
+		var zoomFactor = startDistance / currentDist
+		if canZoom:
+			zoom = startZoom / zoomFactor
+		zoom = clamp(zoom, 0.1, 10)
+	
+	elif touchPoints.size() == 2:
+		var touchPointPositions = touchPoints.values()
+		var currentDist = touchPointPositions[0].distance_to(touchPointPositions[1])
+		
+		var zoomFactor = startDistance / currentDist
+		if canZoom:
+			zoom = startZoom / zoomFactor
+		
+
